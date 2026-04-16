07140a92a245482d6826fb06f808701345d292cb
anaRtem1s <4n4c0d4@proton.me>
2026-04-16 10:08:54 +0200

    Ininitialization

---
+++ script/script.js
@@ -0,0 +1,57 @@
+
+// =================================
+// 🌱 1. Sélection des éléments DOM
+// =================================
+const inputBox = document.querySelector(".input");
+const submitBtn = document.querySelector(".submit");
+const displayBox = document.querySelector(".display"); 
+
+// =================================
+// 🧠 2. Variables globales / état
+// =================================
+const champions = [
+  // Top
+  "Darius", "Garen", "Malphite", "Fiora", "Camille", "Teemo", "Nasus", "Irelia", "Riven", "Sett",
+  // Jungle
+  "Vi", "Warwick", "Hecarim", "Lee Sin", "Ekko", "Amumu", "Shyvana", "Kha'Zix", "Rengar", "Nidalee",
+  // Mid
+  "Yasuo", "Lux", "Syndra", "Zed", "Ahri", "Fizz", "Orianna", "Veigar", "Viktor", "Katarina",
+  // ADC
+  "Jinx", "Caitlyn", "Ezreal", "Jhin", "Miss Fortune", "Vayne", "Ashe", "Draven", "Kai'Sa", "Xayah",
+  // Support
+  "Thresh", "Lulu", "Blitzcrank", "Soraka", "Nautilus", "Leona", "Morgana", "Zilean", "Nami", "Pyke"
+];
+
+// =================================
+// 🎊 3. Fonctions (logique métier)
+// =================================
+// 4. Vérifie si un champion est déjà dans le Roster (et empêcher qu'il soit là 2 fois) INDEX OF
+
+// 5.1. Envoie la valeur dans un tableau
+
+// 5.2. Affiche progressivement les héros dans l'ul qui a la classe liste-champions - PUSH
+
+
+// 7. Crée la fonction afficherRoster()
+
+
+// 8. Quand le roster est complet (5 champions), affiche : ":crossed_swords: Ton équipe est prête, bonne chance sur la faille !"
+
+// 9. Ajoute un bouton "Réinitialiser" qui remet le Roster à 0
+
+
+
+// =================================
+// 🧲 4. Événements (interactions)
+// =================================
+
+// 1. convertir ce tableau en menu déroulant (<select>) grâce à une boucle. 
+
+// 2. Génère les options du select
+
+// 2. Compose ton équipe de 5 champions et voir ton roster s'afficher en temps réel !
+
+
+
+
+// 3. Écoute le clic sur le bouton
